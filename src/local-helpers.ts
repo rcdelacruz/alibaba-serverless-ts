e7c5b651bc8b84dafafcd76ef533d732daa2656a
Ronald DC <rcdelacruz@gmail.com>
2025-04-01 15:48:42 +0800

    Fix TypeScript errors in local-helpers.ts

--- src/local-helpers.ts
+++ src/local-helpers.ts
@@ -1,10 +1,22 @@
 // Simple mock implementation to use in place of Jest if needed
 export const mock = {
   fn: () => {
-    const mockFn = (...args: any[]) => {
+    type MockFunction = {
+      (...args: any[]): any;
+      mock: {
+        calls: any[][];
+        implementation: (((...args: any[]) => any) | null);
+        results: any[];
+        instances: any[];
+      };
+      mockImplementation: (implementation: (...args: any[]) => any) => MockFunction;
+      mockReturnValue: (value: any) => MockFunction;
+    };
+
+    const mockFn = ((...args: any[]): any => {
       mockFn.mock.calls.push(args);
       return mockFn.mock.implementation ? mockFn.mock.implementation(...args) : undefined;
-    };
+    }) as MockFunction;
 
     mockFn.mock = {
       calls: [],
@@ -28,7 +40,7 @@ export const mock = {
 };
 
 // Simple module mocker to use in place of Jest if needed
-export function mockModule(modulePath: string, mockImplementation: any) {
+export function mockModule(modulePath: string, mockImplementation: any): any {
   // In a real implementation, this would intercept require() calls
   // For our purposes, we'll just return the mock implementation
   return mockImplementation;
